44950893388e582b56ee3934ec66949c88cc268d
alinepamplona <alinebpamplona@gmail.com>
2023-08-22 17:08:59 -0300

    Conexão com servidor db via sequilize

--- config.js
+++ config.js
@@ -1,5 +1,15 @@
 require('dotenv').config();
 
+// db
+const dbConfig = {
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: process.env.DB_DIALECT,
+};
+
 // auth
 const secret = process.env.JWT_SECRET || 'esta-es-la-api-burger-queen';
 
@@ -7,6 +17,7 @@ const secret = process.env.JWT_SECRET || 'esta-es-la-api-burger-queen';
 const port =  process.argv[2] || process.env.PORT || 8080;
 
 module.exports = {
+  dbConfig,
   secret,
   port
 };
